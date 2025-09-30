80c7a74b932ef0def250dd9cb88c69dc7b5106f8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-30 10:56:11 -0300

    Finalizando o projeto Sharon - adicionada section de colaboradores com dark mode

---
+++ aula1609/dist/js/darkMode.js
@@ -0,0 +1,6 @@
+const elementHtml = document.querySelector('html');
+const btnMode = document.querySelector('#btnMode');
+
+btnMode.addEventListener('click', () => {
+    elementHtml.classList.toggle('dark');
+})
